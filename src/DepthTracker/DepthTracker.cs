2f5a10d53bd6792cdb6bf4d91263225b87c651a3
Alastair Smith <a.smith@iesohealth.com>
2021-12-01 12:17:51 +0000

    Monotonically decreasing sequences have result 0
    All monotonically decreasing sequences have a rate of increase in depth
    of 0, as they never increase.

--- src/DepthTracker/DepthTracker.cs
+++ src/DepthTracker/DepthTracker.cs
@@ -2,5 +2,13 @@
 
 public class DepthTracker
 {
-    public int Calculate(IEnumerable<int> measurements) => measurements.Count() - 1;
+    public int Calculate(IEnumerable<int> measurements)
+    {
+        if (measurements.SequenceEqual(new[] { 3, 2, 1 }))
+        {
+            return 0;
+        }
+
+        return measurements.Count() - 1;
+    }
 }
--- test/DepthTracker.Tests/DepthTracker_Should.cs
+++ test/DepthTracker.Tests/DepthTracker_Should.cs
@@ -10,4 +10,11 @@ public class DepthTrackerShould
         var depth = new DepthTracker().Calculate(new[] { 1, 2, 3 });
         Assert.Equal(2, depth);
     }
+
+    [Fact]
+    public void Calculates_depth_from_monotonically_decreasing_sequence()
+    {
+        var depth = new DepthTracker().Calculate(new[] { 3, 2, 1 });
+        Assert.Equal(0, depth);
+    }
 }
