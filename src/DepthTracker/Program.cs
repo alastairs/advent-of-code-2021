ca7f57c804f82b23ac89b208d3f8fbf1c23d84c8
Alastair Smith <a.smith@iesohealth.com>
2021-12-01 12:31:01 +0000

    Read & parse the input, calculate depth

--- src/DepthTracker/Program.cs
+++ src/DepthTracker/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async () =>
+{
+    var text = await File.ReadAllLinesAsync("input.txt");
+    var parsed = text.Select(int.Parse);
+    return new global::DepthTracker.DepthTracker().Calculate(parsed);
+});
 
 app.Run();
