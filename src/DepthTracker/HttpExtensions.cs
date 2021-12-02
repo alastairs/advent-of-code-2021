6ac45cff89258c7280755967a89a712bf157ac4d
Alastair Smith <a.smith@iesohealth.com>
2021-12-02 16:26:36 +0000

    Fix bug in HttpExtensions
    Should have R#d that shit.

--- src/DepthTracker/HttpExtensions.cs
+++ src/DepthTracker/HttpExtensions.cs
@@ -5,7 +5,7 @@ internal static class HttpExtensions
     internal static int TryParseQueryValue(this IQueryCollection queryCollection, string name)
     {
         int value = default;
-        var _ = queryCollection.TryGetValue("windowSize", out var values) &&
+        var _ = queryCollection.TryGetValue(name, out var values) &&
             int.TryParse(values.SingleOrDefault(), out value);
 
         return value;
