bfdb2baccbf7a405684d3f8b271d819bb165f6aa
Alastair Smith <a.smith@iesohealth.com>
2021-12-03 16:54:47 +0000

    Copy-paste an array transposition implementation
    Ugh, so boring.
    https://www.codeproject.com/articles/793684/transposing-the-rows-and-columns-of-array-csharp

--- test/DepthTracker.Tests/DiagnosticsReportShould.cs
+++ test/DepthTracker.Tests/DiagnosticsReportShould.cs
@@ -24,10 +24,81 @@ namespace DepthTracker.Tests
             Assert.Equal(input[0].Length, output.Length);
         }
 
-        private T[][] Transpose<T>(T[][] input)
+        [Theory]
+        [MemberData(nameof(TransposeTestCases))]
+        public void Writes_item_to_transposed_location(int[][] input)
         {
-            return Enumerable.Repeat(new T[input.Length], input.Max(i => i.Length)).ToArray();
+            var output = Transpose(input);
+            Assert.Equal(input[0][0], output[0][0]);
+            Assert.Equal(input[0][^1], output[^1][0]);
+            Assert.Equal(input[^1][^1], output[^1][^1]);
         }
+
+        private static T[][] Transpose<T>(IReadOnlyList<T[]> input)
+        {
+            var rowCount = input.Count;
+            var columnCount = input.Max(i => i.Length);
+            var transposed = new T[columnCount][];
+
+            if (rowCount == columnCount)
+            {
+                for (var i = 1; i < rowCount; i++)
+                {
+                    // ReSharper disable once ConstantNullCoalescingCondition - this actually *is* null somehow
+                    transposed[i] ??= new T[rowCount];
+
+                    for (var j = 0; j < rowCount; j++)
+                    {
+                        // ReSharper disable once ConstantNullCoalescingCondition - this actually *is* null somehow
+                        transposed[j] ??= new T[rowCount];
+
+                        (transposed[i][j], transposed[j][i]) =
+                        (input[j][i], input[i][j]);
+                    }
+                }
+            }
+            else
+            {
+                for (var column = 0; column < columnCount; column++)
+                {
+                    transposed[column] = new T[rowCount];
+                    for (var row = 0; row < rowCount; row++)
+                    {
+                        transposed[column][row] = input[row][column];
+                    }
+                }
+            }
+
+            return transposed;
+        }
+
+        public static IEnumerable<IEnumerable<int[][]>> TransposeTestCases => new[] // Test case
+        {
+            new[] // parameters for test case
+            {
+                // The array-of-arrays input
+                new[] { new[] { 0, 1 } }
+            },
+            new[] // parameters for test case
+            {
+                // The array-of-arrays input
+                new[]
+                {
+                    new[] { 0, 1 }, // => 0, 2
+                    new[] { 2, 3 }  // => 1, 3
+                }
+            },
+            new[] // parameters for test case
+            {
+                // The array-of-arrays input
+                new[]
+                {
+                    new[] { 0, 1 }, // => 0, 2, 4
+                    new[] { 2, 3 }, // => 1, 3, 5
+                    new[] { 4, 5 }  // =>
+                }
+            }
+        };
 
         private static IEnumerable<string> Samples => new[]
         {
