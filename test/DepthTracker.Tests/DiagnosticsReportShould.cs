d21bda267faa2e0b8ede35de8132d95bf17b9f2c
Alastair Smith <a.smith@iesohealth.com>
2021-12-03 15:46:34 +0000

    Add acceptance test for the diagnostics report

---
+++ test/DepthTracker.Tests/DiagnosticsReportShould.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DepthTracker.Tests
+{
+    public class DiagnosticsReportShould
+    {
+        [Fact]
+        public void Calculate_diagnostics()
+        {
+            var diagnostics = DiagnosticsReport.Create(Samples);
+
+            Assert.Equal(22, diagnostics.Gamma);
+            Assert.Equal(9, diagnostics.Epsilon);
+        }
+
+        private static IEnumerable<string> Samples => new[]
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+    }
+}
