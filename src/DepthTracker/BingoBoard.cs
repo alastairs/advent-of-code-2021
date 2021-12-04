b31c99a8349c3ec46d5902cdfd3d353d688d4ce9
Alastair Smith <github@alastairsmith.me.uk>
2021-12-04 16:05:33 +0000

    Move Bingo class to its own file

--- src/DepthTracker/BingoBoard.cs
+++ src/DepthTracker/BingoBoard.cs
@@ -2,41 +2,6 @@ using System.Text;
 
 namespace Bingo;
 
-public class Bingo
-{
-    private readonly ICollection<BingoBoard> _boards = new List<BingoBoard>();
-
-    public Bingo(string[] sample)
-    {
-        NumbersToCall = sample[0].Split(",").Select(s => s.Trim()).Select(s => int.Parse(s)).ToList();
-        for (var i = 2; i < sample.Length; i += 6)
-        {
-            _boards.Add(new BingoBoard(sample[i..(i + 5)]));
-        }
-    }
-
-    public (int winningNumber, BingoBoard board) FindWinner()
-    {
-        var winningNumber = 0;
-        var winningBoard = new BingoBoard(Array.Empty<string>());
-        foreach (var called in NumbersToCall)
-        {
-            _boards.ForEach(b => b.Mark(called));
-
-            if ((winningBoard = _boards.FirstOrDefault(b => b.HasWon)) != null)
-            {
-                winningNumber = called;
-                break;
-            }
-        }
-
-        return (winningNumber, winningBoard);
-    }
-
-    public IEnumerable<int> NumbersToCall { get; }
-    public IEnumerable<BingoBoard> Boards => _boards;
-}
-
 public class BingoBoard
 {
     private const int BoardSize = 5;
