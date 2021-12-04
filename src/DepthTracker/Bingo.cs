318c07dc16bc5414c50f54d2f9d3afe6db4eeea1
Alastair Smith <github@alastairsmith.me.uk>
2021-12-04 17:53:48 +0000

    Add more debugging statements

--- src/DepthTracker/Bingo.cs
+++ src/DepthTracker/Bingo.cs
@@ -21,9 +21,19 @@ public class Bingo
         {
             _boards.ForEach(b => b.Mark(called));
 
-            if ((winningBoard = _boards.FirstOrDefault(b => b.HasWon)) != null)
+            if (_boards.Any(b => b.HasWon))
             {
+                winningBoard = _boards.Single(b => b.HasWon);
                 winningNumber = called;
+                Console.WriteLine($"WE HAVE A WINNER!! The {winningNumber} sealed it!");
+
+                var completeRow = Enumerable.Range(0, 5).FirstOrDefault(winningBoard.RowIsComplete, -1);
+                if (completeRow > -1) Console.WriteLine($"Winning board won via complete row {completeRow}");
+
+                Enumerable.Range(0, 5).ForEach(i =>
+                {
+                    Console.WriteLine($"Column {i+1} is complete: {winningBoard.ColumnIsComplete(i)}");
+                });
                 break;
             }
         }
